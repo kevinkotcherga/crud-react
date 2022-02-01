5a467634e7e8af75afe38557df577d3e54e6510c
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-02-01 15:53:43 +0100

    ✨feat(news): limite de 20 caractères ajoutée avec message d'erreur

--- src/pages/News.js
+++ src/pages/News.js
@@ -7,6 +7,7 @@ const News = () => {
   const [newsData, setNewsData] = useState([]);
   const [author, setAuthor] = useState("");
   const [content, setContent] = useState("");
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     getData();
@@ -19,14 +20,20 @@ const News = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    axios.post('http://localhost:3003/articles', {
-      author,
-      content,
-      date: Date.now(),
-    }).then(() => {
-      setAuthor("");
-      setContent("");
-    });
+    if (content.length < 20) {
+        setError(true);
+    } else {
+      axios.post('http://localhost:3003/articles', {
+        author,
+        content,
+        date: Date.now(),
+      }).then(() => {
+        setError(false);
+        setAuthor("");
+        setContent("");
+        getData();
+      });
+    }
   };
 
   return (
@@ -35,7 +42,10 @@ const News = () => {
 
       <form onSubmit={(e) => handleSubmit(e)}>
         <input onChange={(e) => setAuthor(e.target.value)} type='text' placeholder='Nom' value={author} />
-        <textarea onChange={(e) => setContent(e.target.value)} placeholder='Message' value={content}></textarea>
+        <textarea
+        style={{border: error ? "1px solid red" : "1px solid #61dafb"}}
+        onChange={(e) => setContent(e.target.value)} placeholder='Message' value={content}></textarea>
+        {error && <p>Veuillez écrire un minimum de 20 caractères</p>}
         <input type="submit" value="Envoyer" />
       </form>
 
